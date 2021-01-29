62080f66a42a4bd1375c3bb16a64fe1d3d1eeecf
Nikita Prudnikov <prudnikov@mindbox.ru>
2021-01-29 11:21:53 +0300

    cleanup

---
+++ Controllers/FiguresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FiguresDotStore.Controllers
+{
+	public class OrderResult
+	{
+
+	}
+
+	[ApiController]
+	[Route("[controller]")]
+	public class FiguresController : ControllerBase
+	{
+		private readonly ILogger<FiguresController> _logger;
+
+		public FiguresController(ILogger<FiguresController> logger)
+		{
+			_logger = logger;
+		}
+
+		[HttpPost]
+		public ActionResult<OrderResult> Order()
+		{
+			return new EmptyResult();
+		}
+	}
+}
