241ab70418190995fd0645abc258d1baf7fefe3c
likaleina <80872575+likaleina@users.noreply.github.com>
2021-03-18 08:18:20 +0300

    Анжелика Леина

--- Controllers/FiguresController.cs
+++ Controllers/FiguresController.cs
@@ -5,14 +5,17 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
+//везде добавить логирование
 namespace FiguresDotStore.Controllers
 {
+	//объявить в отдельном проекте сервиса по работе с внешним api
 	internal interface IRedisClient
 	{
 		int Get(string type);
 		void Set(string type, int current);
 	}
-	
+	//объявить в отдельном проекте сервиса по работе с внешним api (его нельзя сделать асинхронным?)
+	//в случае, если RedisClient будет недоступен, все ляжет, предусмотреть появление ошибок, и логирование их
 	public static class FiguresStorage
 	{
 		// корректно сконфигурированный и готовый к использованию клиент Редиса
@@ -20,17 +23,19 @@ namespace FiguresDotStore.Controllers
 	
 		public static bool CheckIfAvailable(string type, int count)
 		{
+			//например try-catch или проверять statusCode
 			return RedisClient.Get(type) >= count;
 		}
 
 		public static void Reserve(string type, int count)
 		{
+			//например try-catch или проверять statusCode
 			var current = RedisClient.Get(type);
 
 			RedisClient.Set(type, current - count);
 		}
 	}
-
+	//все классы моделей также вынести в отдельный проект, например Order.Models
 	public class Position
 	{
 		public string Type { get; set; }
@@ -41,25 +46,35 @@ namespace FiguresDotStore.Controllers
 
 		public int Count { get; set; }
 	}
-
+	//Order.Models
 	public class Cart
 	{
+		//public Cart(} {
+		//Positions = new List<Position>();
+		//}
+		//можно заменить List<Position> на ICollection<Position>
 		public List<Position> Positions { get; set; }
 	}
-
+	//Order.Models
 	public class Order
 	{
+		//public Order(} {
+		//Positions = new List<Position>();
+		//}
+		//можно заменить List<Position> на ICollection<Position>
+		//и название тоже мне кажется можно было бы заменить на FigurePositions
 		public List<Figure> Positions { get; set; }
-
+		//не нашла использования
+		//не учтен квадрат
 		public decimal GetTotal() =>
 			Positions.Select(p => p switch
 				{
 					Triangle => (decimal) p.GetArea() * 1.2m,
 					Circle => (decimal) p.GetArea() * 0.9m
-				})
+				}) //если тут будет пусто, то вроде бы получим ошибку, надо проверить на null
 				.Sum();
 	}
-
+	//Order.Models
 	public abstract class Figure
 	{
 		public float SideA { get; set; }
@@ -69,7 +84,7 @@ namespace FiguresDotStore.Controllers
 		public abstract void Validate();
 		public abstract double GetArea();
 	}
-
+	//Order.Models
 	public class Triangle : Figure
 	{
 		public override void Validate()
@@ -89,7 +104,7 @@ namespace FiguresDotStore.Controllers
 		}
 		
 	}
-	
+	//Order.Models
 	public class Square : Figure
 	{
 		public override void Validate()
@@ -103,7 +118,7 @@ namespace FiguresDotStore.Controllers
 
 		public override double GetArea() => SideA * SideA;
 	}
-	
+	//Order.Models
 	public class Circle : Figure
 	{
 		public override void Validate()
@@ -114,7 +129,8 @@ namespace FiguresDotStore.Controllers
 
 		public override double GetArea() => Math.PI * SideA * SideA;
 	}
-
+	//реализацию и сервис объявить там же в отдельном проекте сервиса OrdersCreatorService
+	//либо если это внешнее апи, то там же где и IRedisClient
 	public interface IOrderStorage
 	{
 		// сохраняет оформленный заказ и возвращает сумму
@@ -127,17 +143,24 @@ namespace FiguresDotStore.Controllers
 	{
 		private readonly ILogger<FiguresController> _logger;
 		private readonly IOrderStorage _orderStorage;
+		//private readonly IOrdersCreatorService _orderCreatorService;
 
-		public FiguresController(ILogger<FiguresController> logger, IOrderStorage orderStorage)
+		public FiguresController(ILogger<FiguresController> logger, IOrderStorage orderStorage/*, IOrdersCreatorService orderCreatorService*/)
 		{
 			_logger = logger;
 			_orderStorage = orderStorage;
+			//_orderCreatorService = orderCreatorService;
 		}
 
 		// хотим оформить заказ и получить в ответе его стоимость
 		[HttpPost]
+		//Название не подходит, лучше CreateOrder
+		//не вижу ни одного await... убрать async, либо все методы тоже сделать async-await
 		public async Task<ActionResult> Order(Cart cart)
 		{
+		//try {
+		//логику работы лучше вынести в отдельный класс в отдельном сервисе, например OrdersCreatorService : IOrdersCreatorService 
+		//в Startup.ConfigureServices использовать services.AddTransient<IOrdersCreatorService, OrdersCreatorService>();
 			foreach (var position in cart.Positions)
 			{
 				if (!FiguresStorage.CheckIfAvailable(position.Type, position.Count))
@@ -159,6 +182,8 @@ namespace FiguresDotStore.Controllers
 					figure.SideA = p.SideA;
 					figure.SideB = p.SideB;
 					figure.SideC = p.SideC;
+					//обрабатывать exception, чтобы до пользователя доходило нормальное сообщение, напр. try-catch, 
+					//либо переделать Validate, чтобы он возвращал строку и можно было ее показывать пользоаателю в случае неуспеха
 					figure.Validate();
 					return figure;
 				}).ToList()
@@ -166,12 +191,19 @@ namespace FiguresDotStore.Controllers
 
 			foreach (var position in cart.Positions)
 			{
+			//обработку на случай неуспешного обращения, например вернуть пользователю сообщение
 				FiguresStorage.Reserve(position.Type, position.Count);
 			}
-
+			//добавить проверку на случай, что не удалось провалидировать или зарезервировать, тогда сохранение не должно выполняться
+			//вот тут явно await пропущен
+			//так же нужна проверка на случай, если запрос отвалится в ходе выполнения
 			var result = _orderStorage.Save(order);
-
+		//все что внутри try в отдельный сервис, здесь вызов метода и возврат результата пользователю
 			return new OkObjectResult(result.Result);
+			//}
+			//catch(Exception ex){
+			//_logger.Log($"{ex.Message}\n{ex.StackTrace}";
+			//ну и пользователю что-нить сказать надо, типа попробуйте еще раз
 		}
 	}
 }
